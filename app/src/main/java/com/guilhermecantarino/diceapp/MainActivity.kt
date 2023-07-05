2aec3cd4248eccc4fb0be009893f7492a20e981f
CantarinoG <guilhermecantarino1709@gmail.com>
2023-07-05 18:44:53 -0300

    Minimal design changes

--- app/src/main/java/com/guilhermecantarino/diceapp/MainActivity.kt
+++ app/src/main/java/com/guilhermecantarino/diceapp/MainActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.chip.ChipGroup
 
 class MainActivity : AppCompatActivity() {
+
+    private var isAnimating = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,12 +27,12 @@ class MainActivity : AppCompatActivity() {
         chipGroup.setOnCheckedChangeListener{_, checkedId ->
             textoDaTela.text = resources.getString(R.string.valueNull)
             when(checkedId) {
-                R.id.d4 -> imagemDoDado.setImageResource(R.drawable.d4_1)
-                R.id.d6 -> imagemDoDado.setImageResource(R.drawable.d6_1)
-                R.id.d8 -> imagemDoDado.setImageResource(R.drawable.d8_1)
-                R.id.d10 -> imagemDoDado.setImageResource(R.drawable.d10_1)
-                R.id.d12 -> imagemDoDado.setImageResource(R.drawable.d12_1)
-                R.id.d20 -> imagemDoDado.setImageResource(R.drawable.d20_1)
+                R.id.d4 -> mudaImagem(R.drawable.d4_1, imagemDoDado)
+                R.id.d6 -> mudaImagem(R.drawable.d6_1, imagemDoDado)
+                R.id.d8 -> mudaImagem(R.drawable.d8_1, imagemDoDado)
+                R.id.d10 -> mudaImagem(R.drawable.d10_1, imagemDoDado)
+                R.id.d12 -> mudaImagem(R.drawable.d12_1, imagemDoDado)
+                R.id.d20 -> mudaImagem(R.drawable.d20_1, imagemDoDado)
             }
         }
 
@@ -37,96 +42,96 @@ class MainActivity : AppCompatActivity() {
                     faceSorteada = (1..4).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d4_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d4_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d4_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d4_4)
+                        1 -> mudaImagem(R.drawable.d4_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d4_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d4_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d4_4, imagemDoDado)
                     }
                 }
                 R.id.d6 -> {
                     faceSorteada = (1..6).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d6_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d6_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d6_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d6_4)
-                        5 -> imagemDoDado.setImageResource(R.drawable.d6_5)
-                        6 -> imagemDoDado.setImageResource(R.drawable.d6_6)
+                        1 -> mudaImagem(R.drawable.d6_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d6_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d6_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d6_4, imagemDoDado)
+                        5 -> mudaImagem(R.drawable.d6_5, imagemDoDado)
+                        6 -> mudaImagem(R.drawable.d6_6, imagemDoDado)
                     }
                 }
                 R.id.d8 -> {
                     faceSorteada = (1..8).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d8_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d8_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d8_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d8_4)
-                        5 -> imagemDoDado.setImageResource(R.drawable.d8_5)
-                        6 -> imagemDoDado.setImageResource(R.drawable.d8_6)
-                        7 -> imagemDoDado.setImageResource(R.drawable.d8_7)
-                        8 -> imagemDoDado.setImageResource(R.drawable.d8_8)
+                        1 -> mudaImagem(R.drawable.d8_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d8_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d8_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d8_4, imagemDoDado)
+                        5 -> mudaImagem(R.drawable.d8_5, imagemDoDado)
+                        6 -> mudaImagem(R.drawable.d8_6, imagemDoDado)
+                        7 -> mudaImagem(R.drawable.d8_7, imagemDoDado)
+                        8 -> mudaImagem(R.drawable.d8_8, imagemDoDado)
                     }
                 }
                 R.id.d10 -> {
                     faceSorteada = (1..10).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d10_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d10_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d10_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d10_4)
-                        5 -> imagemDoDado.setImageResource(R.drawable.d10_5)
-                        6 -> imagemDoDado.setImageResource(R.drawable.d10_6)
-                        7 -> imagemDoDado.setImageResource(R.drawable.d10_7)
-                        8 -> imagemDoDado.setImageResource(R.drawable.d10_8)
-                        9 -> imagemDoDado.setImageResource(R.drawable.d10_9)
-                        10 -> imagemDoDado.setImageResource(R.drawable.d10_10)
+                        1 -> mudaImagem(R.drawable.d10_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d10_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d10_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d10_4, imagemDoDado)
+                        5 -> mudaImagem(R.drawable.d10_5, imagemDoDado)
+                        6 -> mudaImagem(R.drawable.d10_6, imagemDoDado)
+                        7 -> mudaImagem(R.drawable.d10_7, imagemDoDado)
+                        8 -> mudaImagem(R.drawable.d10_8, imagemDoDado)
+                        9 -> mudaImagem(R.drawable.d10_9, imagemDoDado)
+                        10 -> mudaImagem(R.drawable.d10_10, imagemDoDado)
                     }
                 }
                 R.id.d12 -> {
                     faceSorteada = (1..12).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d12_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d12_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d12_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d12_4)
-                        5 -> imagemDoDado.setImageResource(R.drawable.d12_5)
-                        6 -> imagemDoDado.setImageResource(R.drawable.d12_6)
-                        7 -> imagemDoDado.setImageResource(R.drawable.d12_7)
-                        8 -> imagemDoDado.setImageResource(R.drawable.d12_8)
-                        9 -> imagemDoDado.setImageResource(R.drawable.d12_9)
-                        10 -> imagemDoDado.setImageResource(R.drawable.d12_10)
-                        11 -> imagemDoDado.setImageResource(R.drawable.d12_11)
-                        12 -> imagemDoDado.setImageResource(R.drawable.d12_12)
+                        1 -> mudaImagem(R.drawable.d12_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d12_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d12_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d12_4, imagemDoDado)
+                        5 -> mudaImagem(R.drawable.d12_5, imagemDoDado)
+                        6 -> mudaImagem(R.drawable.d12_6, imagemDoDado)
+                        7 -> mudaImagem(R.drawable.d12_7, imagemDoDado)
+                        8 -> mudaImagem(R.drawable.d12_8, imagemDoDado)
+                        9 -> mudaImagem(R.drawable.d12_9, imagemDoDado)
+                        10 -> mudaImagem(R.drawable.d12_10, imagemDoDado)
+                        11 -> mudaImagem(R.drawable.d12_11, imagemDoDado)
+                        12 -> mudaImagem(R.drawable.d12_12, imagemDoDado)
                     }
                 }
                 R.id.d20 -> {
                     faceSorteada = (1..12).random()
                     textoDaTela.text = resources.getString(R.string.value, faceSorteada.toString())
                     when(faceSorteada){
-                        1 -> imagemDoDado.setImageResource(R.drawable.d20_1)
-                        2 -> imagemDoDado.setImageResource(R.drawable.d20_2)
-                        3 -> imagemDoDado.setImageResource(R.drawable.d20_3)
-                        4 -> imagemDoDado.setImageResource(R.drawable.d20_4)
-                        5 -> imagemDoDado.setImageResource(R.drawable.d20_5)
-                        6 -> imagemDoDado.setImageResource(R.drawable.d20_6)
-                        7 -> imagemDoDado.setImageResource(R.drawable.d20_7)
-                        8 -> imagemDoDado.setImageResource(R.drawable.d20_8)
-                        9 -> imagemDoDado.setImageResource(R.drawable.d20_9)
-                        10 -> imagemDoDado.setImageResource(R.drawable.d20_10)
-                        11 -> imagemDoDado.setImageResource(R.drawable.d20_11)
-                        12 -> imagemDoDado.setImageResource(R.drawable.d20_12)
-                        13 -> imagemDoDado.setImageResource(R.drawable.d20_13)
-                        14 -> imagemDoDado.setImageResource(R.drawable.d20_14)
-                        15 -> imagemDoDado.setImageResource(R.drawable.d20_15)
-                        16 -> imagemDoDado.setImageResource(R.drawable.d20_16)
-                        17 -> imagemDoDado.setImageResource(R.drawable.d20_17)
-                        18 -> imagemDoDado.setImageResource(R.drawable.d20_18)
-                        19 -> imagemDoDado.setImageResource(R.drawable.d20_19)
-                        20 -> imagemDoDado.setImageResource(R.drawable.d20_20)
+                        1 -> mudaImagem(R.drawable.d20_1, imagemDoDado)
+                        2 -> mudaImagem(R.drawable.d20_2, imagemDoDado)
+                        3 -> mudaImagem(R.drawable.d20_3, imagemDoDado)
+                        4 -> mudaImagem(R.drawable.d20_4, imagemDoDado)
+                        5 -> mudaImagem(R.drawable.d20_5, imagemDoDado)
+                        6 -> mudaImagem(R.drawable.d20_6, imagemDoDado)
+                        7 -> mudaImagem(R.drawable.d20_7, imagemDoDado)
+                        8 -> mudaImagem(R.drawable.d20_8, imagemDoDado)
+                        9 -> mudaImagem(R.drawable.d20_9, imagemDoDado)
+                        10 -> mudaImagem(R.drawable.d20_10, imagemDoDado)
+                        11 -> mudaImagem(R.drawable.d20_11, imagemDoDado)
+                        12 -> mudaImagem(R.drawable.d20_12, imagemDoDado)
+                        13 -> mudaImagem(R.drawable.d20_13, imagemDoDado)
+                        14 -> mudaImagem(R.drawable.d20_14, imagemDoDado)
+                        15 -> mudaImagem(R.drawable.d20_15, imagemDoDado)
+                        16 -> mudaImagem(R.drawable.d20_16, imagemDoDado)
+                        17 -> mudaImagem(R.drawable.d20_17, imagemDoDado)
+                        18 -> mudaImagem(R.drawable.d20_18, imagemDoDado)
+                        19 -> mudaImagem(R.drawable.d20_19, imagemDoDado)
+                        20 -> mudaImagem(R.drawable.d20_20, imagemDoDado)
                     }
                 }
             }
@@ -134,4 +139,22 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun mudaImagem(idImagem: Int, imagemDado: ImageView){
+        if(!isAnimating) {
+            isAnimating = true
+            imagemDado.animate().apply {
+                duration = 700
+                rotationXBy(360f)
+                alpha(0f)
+            }.withEndAction{
+                imagemDado.animate().apply {
+                    alpha(1f)
+                }
+                imagemDado.setImageResource(idImagem)
+                isAnimating = false
+            }.start()
+        }
+    }
+
 }
